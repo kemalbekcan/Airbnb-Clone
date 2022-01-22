7bc8c4d7aee82447ae22e85e926f38e6ec8aaaf5
kemalbekcan <kemalbekcan@gmail.com>
2022-01-22 21:09:23 +0300

    Refactor `webpack`

--- Client/webpack.config.js
+++ Client/webpack.config.js
@@ -1,34 +1,36 @@
-const path = require("path");
+const path = require('path');
 // eslint-disable-next-line import/no-unresolved, import/no-extraneous-dependencies
-const webpack = require("webpack");
+const webpack = require('webpack');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
+  watch: true,
   output: {
-    path: path.join(__dirname, "dist"),
-    publicPath: "/",
-    filename: "bundle.js",
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, "public"),
+      directory: path.join(__dirname, 'public'),
     },
     compress: true,
     port: 8080,
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
-  devtool: "source-map",
+  devtool: 'source-map',
 };
