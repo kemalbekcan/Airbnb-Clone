1cf14a136a81cb47c9eb1a5604ce8438f5a96104
kemalbekcan <kemalbekcan@gmail.com>
2022-01-22 21:09:45 +0300

    Create `Header` component

---
+++ Client/src/Components/Header.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+function Header() {
+  return (
+    <Fragment>
+      <div>COVID-19 çerçevesinde aldığımız önlemler hakkındaki en güncel bilgileri öğrenin</div>
+      <div>
+        <div>Logo</div>
+        <div>Menu</div>
+        <div>Profile</div>
+      </div>
+    </Fragment>
+  );
+}
+export default Header;
