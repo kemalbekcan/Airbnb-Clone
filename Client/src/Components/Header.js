db6756c3706b60221a59e659ada8e733485979c4
kemalbekcan <kemalbekcan@gmail.com>
2022-01-25 02:11:58 +0300

    Created `Header` component

--- Client/src/Components/Header.js
+++ Client/src/Components/Header.js
@@ -2,8 +2,8 @@ import React, { Fragment } from 'react';
 function Header() {
   return (
     <Fragment>
-      <div>COVID-19 çerçevesinde aldığımız önlemler hakkındaki en güncel bilgileri öğrenin</div>
       <div>
+        <h3>COVID-19 çerçevesinde aldığımız önlemler hakkındaki en güncel bilgileri öğrenin</h3>
         <div>Logo</div>
         <div>Menu</div>
         <div>Profile</div>
