5a9b5810dd1fda1ef30b9c2a191fac4c41eb0687
kemalbekcan <kemalbekcan@gmail.com>
2022-01-25 02:09:05 +0300

    Changed `index.js`

--- Client/src/index.js
+++ Client/src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './Styles/style.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
